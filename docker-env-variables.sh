6d1b8bb0ddccae3a4cd20140ba8a9d89232d18b7
papalozarou <github@lozworld.com>
2023-10-18 22:26:32 +0100

    removed sh command from docker commands

--- docker-env-variables.sh
+++ docker-env-variables.sh
@@ -3,3 +3,173 @@
 #-------------------------------------------------------------------------------
 # Functions for getting, reading and setting docker environment variables.
 #-------------------------------------------------------------------------------
+
+#-------------------------------------------------------------------------------
+# Asks the user if they want to re-set an existing docker environment variable. 
+# Takes two mandatory arguements:
+# 
+# 1. "${1:?}" – the environment file; and
+# 2. "${2:?}" – the existing variable.
+# 
+# If a user choses "y/Y" the variable is set via "setEnvVariable".
+#-------------------------------------------------------------------------------
+changeDockerEnvVariable () {
+  local ENV_FILE="${1:?}"
+  local ENV_VARIABLE="${2:?}"
+  local ENV_VALUE="$(readDockerEnvVariable "$ENV_FILE" "$ENV_VARIABLE")"
+
+  echoComment "The current value of $ENV_VARIABLE is:"
+  echoSeparator
+  echoComment "$ENV_VALUE"
+  echoSeparator
+
+  promptForUserInput "Do you want to change $ENV_VARIABLE?"
+  ENV_VARIABLE_SET_YN="$(getUserInput)"
+
+  if [ "$ENV_VARIABLE_SET_YN" = 'y' -o  "$ENV_VARIABLE_SET_YN" = 'Y' ]; then
+    promptForUserInput "What value do you require for $ENV_VARIABLE?"
+    ENV_VALUE="$(getUserInput)"
+
+    setDockerEnvVariable "$ENV_FILE" "$ENV_VARIABLE" "$ENV_VALUE"
+  else
+    echoComment "No changes to $ENV_VARIABLE made."
+  fi
+}
+
+#-------------------------------------------------------------------------------
+# Checks for and sets multiple docker environment variables. Takes at least two 
+# arguements:
+# 
+# 1. "${1:?}" – the environment file; and
+# 2. "$i" – one or more environment variables to set.
+# 
+# The function takes the first argument and stores it as the environment 
+# variable file. It then shifts the argument position by 1, and loops through 
+# each of the rest of the arguments. As per:
+# 
+# - https://unix.stackexchange.com/a/225951
+#-------------------------------------------------------------------------------
+checkAndSetDockerEnvVariables () {
+  local ENV_FILE="${1:?}"
+
+  shift
+
+  for i; do
+    local ENV_VARIABLE_TF="$(checkIfDockerEnvVariableSet "$ENV_FILE" "$i")"
+
+    echoComment "Checking to see if $i is set…"
+    echoComment "Check returned $ENV_VARIABLE_TF."
+
+    changeDockerEnvVariable "$ENV_FILE" "$i"
+  done
+}
+
+#-------------------------------------------------------------------------------
+# Checks whether a docker environment variable has been set. Takes two mandatory 
+# arguements:
+# 
+# 1. "${1:?}" – the environment file; and
+# 2. "${2:?}" – the required variable.
+# 
+# Returns true if the variable is set, returns false if it is unset or is set to
+# "${DKR_ENV: $VAR}".
+#-------------------------------------------------------------------------------
+checkIfDockerEnvVariableSet () {
+  local ENV_FILE="${1:?}"
+  local ENV_VARIABLE="${2:?}"
+  local ENV_VALUE="$(readDockerEnvVariable "$ENV_FILE" "$ENV_VARIABLE")"
+
+  if [ -z "$ENV_VALUE" -o "$ENV_VALUE" = "\${DKR_ENV: \$$ENV_VARIABLE}" ]; then
+    echo false
+  else
+    echo true
+  fi
+}
+
+#-------------------------------------------------------------------------------
+# Reads an environment variable from a file. Takes two mandatory arguments:
+# 
+# 1. "${1:?}" - the file containing the environment value; and
+# 2. "${2:?}" - an environment variable to check.
+# 
+# The variable and value are read from the file with "grep" as a single string, 
+# then piped to "cut", which uses the equals sign as a field delimter to return 
+# the second field, i.e. the value. As per:
+# 
+# - https://unix.stackexchange.com/a/312281
+# 
+# N.B.
+# "grep" is passed the flag "-m 1" to match only the first instance of the 
+# variable. As per:
+# 
+# - https://stackoverflow.com/a/14093511
+#-------------------------------------------------------------------------------
+readDockerEnvVariable () {
+  local ENV_FILE="${1:?}"
+  local ENV_VARIABLE="${2:?}"
+  local ENV_VALUE="$(grep -m 1 "$ENV_VARIABLE=" "$ENV_FILE" | cut -d'=' -f2)"
+
+  echo "$ENV_VALUE"
+}
+
+#-------------------------------------------------------------------------------
+# Substitutes instances of "${DKR_ENV: $ENV_VARIABLE}" with correct values. 
+# Takes three mandatory arguments:
+# 
+# 1. "${1:?}" - the file containing the environment value;
+# 2. "${2:?}" - an environment variable to replace; and
+# 3. "${3:?}" - the value of the variable.
+# 
+# The "sed" delimiter is "|" in this instance as both "/" and "@" will be
+# returned as part of string hashing or present in email addresses.
+# 
+# N.B.
+# This only replaces the above string, and is used on configuration files, not 
+# ".env" files.
+#-------------------------------------------------------------------------------
+replaceDockerEnvPlaceholderVariable () {
+  local ENV_FILE="${1:?}"
+  local ENV_VARIABLE="${2:?}"
+  local ENV_VALUE="${3:?}"
+
+  echoComment "Replacing placholder $ENV_VARIABLE in:"
+  echoComment "$ENV_FILE"
+  sed -i 's|\${DKR_ENV: \$'"$ENV_VARIABLE"'}|'"$ENV_VALUE"'|g' "$ENV_FILE"
+
+  echoComment 'Checking variables have been replaced.'
+  echoSeparator
+  grep "$ENV_VALUE" "$ENV_FILE"
+  echoSeparator
+  echoComment "Placeholder variable replaced."
+}
+
+#-------------------------------------------------------------------------------
+# Substitutes the respective "${DKR_ENV: $VARIABLE}" with "$VALUE" in a given 
+# "$FILE". Takes three mandatory arguments:
+# 
+# 1. "{1:?}" - the filepath of the file to change;
+# 2. "{2:?}" - the variable name; and
+# 3. "{3:?}" - the variable value.
+# 
+# The function uses "sed" to replace the variable with the value in the 
+# specified document. As per:
+# 
+# - https://stackoverflow.com/a/11245501
+# 
+# N.B.
+# It's necessary to use single and double quotes, as well as escaped characters, 
+# to enable "sed" to parse the variables where required.
+#-------------------------------------------------------------------------------
+setDockerEnvVariable () {
+  local ENV_FILE="${1:?}"
+  local ENV_VARIABLE="${2:?}"
+  local ENV_VALUE="${3:?}"
+
+  echoComment "Setting $ENV_VARIABLE to $ENV_VALUE in:"
+  echoComment "$ENV_FILE"
+  sed -i '/'"$ENV_VARIABLE"='/c\\'"$ENV_VARIABLE=$ENV_VALUE" "$ENV_FILE"
+  echoSeparator
+  grep "$ENV_VALUE" "$ENV_FILE"
+  echoSeparator
+  echoComment 'Variable updated.'
+}
---
+++ docker-images.sh
@@ -0,0 +1,57 @@
+#!/bin/sh
+
+#-------------------------------------------------------------------------------
+# Functions for manipulating docker images.
+#-------------------------------------------------------------------------------
+
+#-------------------------------------------------------------------------------
+# Creates one or more images. Takes at least two or more arguments:
+# 
+# 1. "${1:?}" - the compose file; and
+# 2. "$i" – one or more images to be built.
+# 
+# The function takes the first argument and stores it as the compose file. It 
+# then shifts the argument position by 1, and loops through each of the rest of
+# the arguments. As per:
+# 
+# - https://unix.stackexchange.com/a/225951
+#-------------------------------------------------------------------------------
+buildDockerImages () {
+  local COMPOSE_FILE="${1:?}"
+
+  shift
+
+  for i; do
+    echoComment "Building $i."
+    echoSeparator
+    controlDockerService "$COMPOSE_FILE" "build" "$i" "--no-cache"
+    echoSeparator
+    echoComment 'Assuming there are no errors above, the image was built.'
+  done
+}
+
+#-------------------------------------------------------------------------------
+# Lists images.
+#-------------------------------------------------------------------------------
+listDockerImages () {
+  echoComment 'Listing docker images.'
+  echoSeparator
+  docker image ls
+  echoSeparator
+}
+
+#-------------------------------------------------------------------------------
+# Removes one or more images. Takes at least one or more arguments:
+# 
+# 1. "$@" - the image or images to be removed.
+#-------------------------------------------------------------------------------
+removeDockerImages () {
+  echoComment 'Removing the following docker images:'
+  echoComment "$@"
+
+  for IMAGE in $@; do
+    echoSeparator
+    docker rm "$IMAGE"
+    echoSeparator
+  done
+}
