4b88b36eb88e0e1fd8b6b07c47db24daf5f5428e
papalozarou <github@lozworld.com>
2024-12-19 13:51:06 +0000

    Added random secret generation

--- docker-secrets.sh
+++ docker-secrets.sh
@@ -21,3 +21,24 @@ createDockerSecretsDir () {
     setPermissions '600' "$DOCKER_SECRETS_DIR"
   fi
 }
+
+#-------------------------------------------------------------------------------
+# Generates secrets, using a random string. Takes at least one mandatory 
+# argument:
+# 
+# 1. "$@" - the name(s) of the secrets file(s), all lowercase.
+#-------------------------------------------------------------------------------
+generateRandomDockerSecrets () {
+  for FILE in "$@"; do
+    local SECRET_FILE="$DOCKER_SECRETS_DIR/$FILE"
+    local SECRET_VALUE="$(generateRandomString)"
+
+    echoComment 'Generating a secret file at:'
+    echoComment "$SECRET_FILE"
+    echo "$SECRET_VALUE" >> "$SECRET_FILE"
+
+    setPermissions '644' "$SECRET_FILE"
+  done
+
+  listDirectories "$DOCKER_SECRETS_DIR"
+}
