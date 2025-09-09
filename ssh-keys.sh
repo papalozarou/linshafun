1a1805f172492c1cdabbcf6d8e2861d8483e4249
papalozarou <github@lozworld.com>
2025-09-09 11:18:13 +0100

    Separated out ssh-config related functions

--- ssh-keys.sh
+++ ssh-keys.sh
@@ -26,44 +26,6 @@ addKeyToAuthorizedKeys () {
 }
 
 #-------------------------------------------------------------------------------
-# Adds a host to "~/.ssh/config". Takes four arguments:
-# 
-# 1. "${1:?}" - the name of the host;
-# 2. "$2" - the address of the host, either an IP address or a hostname;
-# 3. "$3" - the ssh port for the host; and
-# 4. "$4" - the user used to login to the host.
-#
-# The function tests the three non-mandatory arguments to see if they are set,
-# and if so adds them to the host config.
-#
-# N.B.
-# Although these variables may exist in the global scope if "getSshHostDetails" 
-# has been used, this allows fixed details to be passed in if required.
-#-------------------------------------------------------------------------------
-addHostToSshConfig () {
-  local HOST="${1:?}"
-  local HOSTNAME="$2"
-  local PORT="$3"
-  local USER="$4"
-  local IDENTITY_FILE="$HOST"
-
-  printComment "Adding host, $HOST, to ssh config file at:"
-  printComment "$SSH_CONF"
-
-  {
-    echo "Host $HOST"
-    [ -n "$HOSTNAME" ] && echo "  Hostname $HOSTNAME"
-    [ -n "$PORT" ] && echo "  Port $PORT"
-    [ -n "$USER" ] && echo "  User $USER"
-    [ -n "$IDENTITY_FILE" ] && echo "  IdentityFile ~/.ssh/$IDENTITY_FILE"
-    echo
-  } >> "$SSH_CONF"
-
-  setPermissions 600 "$SSH_CONF"
-  setOwner "$SUDO_USER" "$SSH_CONF"
-}
-
-#-------------------------------------------------------------------------------
 # Checks to see if the "~/.ssh/authorized_keys" file exist and creates it if not.
 #-------------------------------------------------------------------------------
 checkForAndCreateAuthorizedKeys () {
@@ -81,56 +43,6 @@ checkForAndCreateAuthorizedKeys () {
 
     setPermissions 600 "$SSH_AUTH_KEYS"
     setOwner "$SUDO_USER" "$SSH_AUTH_KEYS"
-  fi
-}
-
-#-------------------------------------------------------------------------------
-# Checks to see if the "~/.ssh" directory exist and creates it if not.
-#-------------------------------------------------------------------------------
-checkForAndCreateSshDir () {
-  local SSH_DIR_TF="$(checkForFileOrDirectory "$SSH_DIR")"
-
-  printComment 'Checking for an "~/.ssh" directory at:'
-  printComment "$SSH_DIR"
-
-  if [ "$SSH_DIR_TF" = true ]; then
-    printComment 'The "~/.ssh" directory already exists.' true
-  elif [ "$SSH_DIR_TF" = false ]; then 
-    printComment 'Creating an "~/.ssh" directory at:'
-    printComment "$SSH_DIR"
-    createDirectory "$SSH_DIR"
-
-    setPermissions 700 "$SSH_DIR"
-    setOwner "$SUDO_USER" "$SSH_DIR"
-  fi
-}
-
-#-------------------------------------------------------------------------------
-# Checks to see if the "~/.ssh/config" file exist and creates it if not, adding
-# a basic config to the created file.
-#-------------------------------------------------------------------------------
-checkForAndCreateSshConfig () {
-  local SSH_CONF_TF="$(checkForFileOrDirectory "$SSH_CONF")"
-
-  printComment 'Checking for an ssh config file at:'
-  printComment "$SSH_CONF"
-
-  if [ "$SSH_CONF_TF" = true ]; then
-    printComment 'The ssh config file already exists.' true
-  elif [ "$SSH_CONF_TF" = false ]; then
-    printComment 'Creating an ssh config file at:'
-    printComment "$SSH_CONF"
-    createFiles "$SSH_CONF"
-
-    cat <<EOF > "$SSH_CONF"
-Host *
-  AddKeysToAgent yes
-  IdentitiesOnly yes
-  
-EOF
-
-    setPermissions 600 "$SSH_CONF"
-    setOwner "$SUDO_USER" "$SSH_CONF"
   fi
 }
 
@@ -162,24 +74,6 @@ generateSshKey () {
   setOwner "$SUDO_USER" "$KEY_PATH.pub"
 
   listDirectories "$KEY_PATH"
-}
-
-#-------------------------------------------------------------------------------
-# Gets details of an SSH host for adding to the ssh config file. Details are 
-# stored in global variables to allow other functions to use them.
-#-------------------------------------------------------------------------------
-getSshHostDetails () {
-  promptForUserInput 'What is the name of the host you want to add?'
-  SSH_HOST="$(getUserInput)"
-
-  promptForUserInput 'What is the ip address of the host you want to add?'
-  SSH_HOSTNAME="$(getUserInput)"  
-
-  promptForUserInput 'What is the ssh port for the host you want to add?'
-  SSH_PORT="$(getUserInput)"
-
-  promptForUserInput 'What is the name of the ssh user for the host you want to add?'
-  SSH_USER="$(getUserInput)"  
 }
 
 #-------------------------------------------------------------------------------
@@ -221,7 +115,7 @@ printPublicKeyUsage () {
 }
 
 #-------------------------------------------------------------------------------
-# Removes a generated private key. Takes one mandatory argument:
+# Removes a private key. Takes one mandatory argument:
 # 
 # 1. "${1:?}" - the ssh key file, including directory path.
 #-------------------------------------------------------------------------------
@@ -232,4 +126,18 @@ removePrivateSshKey () {
   printComment "$KEY"
   rm "$KEY"
   printComment "Private key removed."
+}
+
+#-------------------------------------------------------------------------------
+# Removes a public key. Takes one mandatory argument:
+# 
+# 1. "${1:?}" - the ssh key file, including directory path.
+#-------------------------------------------------------------------------------
+removePublicSshKey () {
+  local KEY="${1:?}"
+
+  printComment 'Removing the public key at:'
+  printComment "$KEY"
+  rm "$KEY"
+  printComment "Public key removed."
 }
