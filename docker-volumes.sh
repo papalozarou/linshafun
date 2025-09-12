b5c3b6b62ffe801ae05a84de1da42dc7ad8c655a
papalozarou <github@lozworld.com>
2025-09-12 16:25:57 +0100

    Amended comments, tidied up variables

--- docker-volumes.sh
+++ docker-volumes.sh
@@ -7,14 +7,18 @@
 #-------------------------------------------------------------------------------
 # Lists the contents of docker volumes. Takes one or more arguments
 # 
-# 1. "${1:?}" – the prefix for the volumes; and
-# 2. "$i" – the volume or volumes to be listed, including prefix
+# 1. "${1:?}" – the voolume prefix, usually the compose "name" attribute; and
+# 2. "$@" – one or more volumes to be listed, excluding prefix.
+#
+# N.B.
+# The prefix is converted to lower case and an underscore is added to the end to
+# enable "$PREFIX$VOLUME" to produce "prefix_volume".
 #-------------------------------------------------------------------------------
-listVolumeContents () {
-  local PREFIX="${1:?}"
+listDockerVolumeContents () {
+  local PREFIX="$(changeCase "${1:?}" 'lower')_"
   
-  for i; do
-    local VOLUME="$PREFIX_$i"
+  for VOLUME in "$@"; do
+    local VOLUME="$PREFIX$VOLUME"
 
     printComment 'Listing contents of the following docker volume:'
     printComment "$VOLUME"
@@ -30,7 +34,11 @@ listVolumeContents () {
 #
 # 1. "${1:?}" – the action to perform; 
 # 2. "${2:?}" – the prefix for the volumes; and
-# 2. "$i" – one ore more volumes.
+# 2. "$@" – one ore more volumes, excluding prefix.
+#
+# N.B.
+# The prefix is converted to lower case and an underscore is added to the end to
+# enable "$PREFIX$VOLUME" to produce "prefix_volume".
 #-------------------------------------------------------------------------------
 manageDockerVolumes () {
   local ACTION="${1:?}"
@@ -40,24 +48,21 @@ manageDockerVolumes () {
 
   shift 2
 
-  for i; do
-    local VOLUME="$PREFIX$i"
+  for VOLUME in "$@"; do
+    local VOLUME="$PREFIX$VOLUME"
 
     printComment "Performing $ACTION on the following docker volume:"
     printComment "$VOLUME"
     printSeparator
 
     if [ "$ACTION" = 'rm' ]; then 
-      docker volume $ACTION -f $VOLUME
+      docker volume "$ACTION" -f "$VOLUME"
     else
-      docker volume $ACTION $VOLUME
+      docker volume "$ACTION" "$VOLUME"
     fi
 
     printSeparator
+    ACTION="$(changeCase "$ACTION" 'sentence')"
     printComment "$ACTION performed on $VOLUME."
   done
-
-  printSeparator
-  docker image ls
-  printSeparator
 }
